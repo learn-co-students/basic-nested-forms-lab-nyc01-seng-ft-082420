d87fbbf100306eb8453fc5cd2f43ef071f413043
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-16 22:46:53 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
 end
+
+
+# # Recipe = {title: "chocolate cake", 0 => {name: 'sugar', quantity: '1 cup', 1 => {
+#    name: 'vanilla', quantity: '1 tablespoon'}}}
